a09d765f16f422cb2a83426a4ecbd9b5da2135a7
johnkaick <john.kaick@gmail.com>
2020-11-28 10:56:28 -0300

    feat: add varible env in url dv

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('postgres://test:test@localhost:5432/db_test');
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://test:test@localhost:5432/db_test',
+);
 
 export default sequelize;
